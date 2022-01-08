ac194e6b86fae143e6bf9b83de9822f23fb000e7
XPLCT <rafaeldizon6969@gmail.com>
2022-01-08 21:56:24 +0800

    improvedgraphics

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,7 +3,7 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
-    setSize (600, 400);
+    setSize (600, 600);
 }
 
 MainComponent::~MainComponent()
@@ -16,9 +16,9 @@ void MainComponent::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setFont (juce::Font (32.0f));
+    g.setFont (juce::Font (40.0f));
     g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.drawText ("Yo X, you made this?", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
